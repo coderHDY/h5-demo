0428a5d5eb5e5d0ec89a80571acaa25e54b19902
coder-HDY <d_huang@dreamarts.co.jp>
2022-08-30 21:29:20 +0800

    目录调整

--- src/基础010-笑话/index.js
+++ src/基础010-笑话/index.js
@@ -1,6 +1,8 @@
 const joke = document.querySelector(".joke");
+const jokeTrans = document.querySelector(".joke-trans");
 const button = document.querySelector(".button");
 
+const trans = async s => await (await fetch(`https://api.66mz8.com/api/translation.php?info=${s.replace(/[^\w\s]+/g, ".")}`)).json()
 const changeJoke = async () => {
   const config = {
     headers: {
@@ -9,6 +11,9 @@ const changeJoke = async () => {
   }
   const jokeData = await (await fetch("https://icanhazdadjoke.com", config)).json();
   joke.innerText = jokeData.joke;
+
+  const joketrans = await trans(jokeData.joke);
+  jokeTrans.innerText = joketrans.fanyi ?? "";
 }
 button.addEventListener("click", changeJoke);
 changeJoke();
