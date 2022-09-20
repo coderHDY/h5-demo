8b88a759aafacf31ecf2968095e2f25a5c4e580a
coderHDY <coderHDY@users.noreply.github.com>
2022-09-20 07:47:53 +0000

    自动部署 b962d2d6978c095846fefe4d6370e553a961594f

---
+++ 基础21-图片拖动2/index.js
@@ -0,0 +1,35 @@
+const container = document.querySelector(".container");
+const iptContainer = document.querySelector(".ipt-container");
+
+const host = 'https://hdy.gh520.xyz';
+// const host = 'http://127.0.0.1:8080';
+
+function dragOver(e) {
+  e.preventDefault();
+}
+function drop(e) {
+  e.preventDefault();
+  const file = e.dataTransfer.files[0];
+  uploadAndUpdate(file);
+}
+function change() {
+  const file = this.files[0];
+  uploadAndUpdate(file);
+}
+const uploadAndUpdate = async (file) => {
+  const formData = new FormData();
+  formData.append("file", file);
+  const { filename } = await fetch(`${host}/api/uploadFile`, {
+      method: "POST",
+      body: formData,
+  }).then(res => res.ok ? res.json() : {});
+  if (filename) {
+    const img = document.createElement("img");
+    img.src = `${host}/files/${filename}`;
+    container.appendChild(img);
+  }
+}
+
+iptContainer.addEventListener("dragover", dragOver);
+iptContainer.addEventListener("drop", drop);
+iptContainer.addEventListener("change", change);
