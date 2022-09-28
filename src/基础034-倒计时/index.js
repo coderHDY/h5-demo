0e687fd64720c7b6d29c59b8ae7641d3a574d1f4
coder-HDY <d_huang@dreamarts.co.jp>
2022-09-28 14:14:00 +0800

    倒计时

---
+++ src/基础034-倒计时/index.js
@@ -0,0 +1,19 @@
+const loading = document.querySelector(".loading");
+const wrapper = document.querySelector(".wrapper");
+const reloadBtn = document.querySelector(".reload-btn");
+
+let timer = 3;
+
+loading.addEventListener("animationstart", (e) => {
+  let calcTime = timer;
+  loading.innerText = calcTime--;
+  const interval = setInterval(() => loading.innerText = calcTime--, 1000);
+  setTimeout(() => {
+    wrapper.classList.remove("running");
+    clearInterval(interval);
+  }, (calcTime + 2) * 1000);
+})
+
+reloadBtn.addEventListener("click", () => {
+  wrapper.classList.add("running");
+})
