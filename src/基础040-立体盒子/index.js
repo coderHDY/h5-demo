abdd5d6570104aab09856188aa977986b74af3b5
coder-HDY <d_huang@dreamarts.co.jp>
2022-09-29 12:57:57 +0800

    魔术方块

---
+++ src/基础040-立体盒子/index.js
@@ -0,0 +1,6 @@
+// const magic = document.querySelector(".magic");
+// const wrapper = document.querySelector(".wrapper");
+
+// magic.addEventListener("click", () => {
+//   wrapper.classList.toggle("small")
+// })
