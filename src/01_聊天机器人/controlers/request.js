67595e5df612b2212c24b77e2d9d2ee16f2cc363
coderHDY <huangdeyu2020@163.com>
2022-10-05 23:34:02 +0800

    port修改

--- src/01_聊天机器人/controlers/request.js
+++ src/01_聊天机器人/controlers/request.js
@@ -1,4 +1,5 @@
-const BASE_URL = "https://hdy.gh520.xyz";
+// const BASE_URL = "https://hdy.gh520.xyz";
+const BASE_URL = "http://164.88.255.3:8070";
 
 const chatRobot = async (val) => await (await fetch(`${BASE_URL}/api/robot/${val}`, {
     method: "GET",
