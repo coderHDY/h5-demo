24b03242d230c1fbcc5d1a4ac4e24d126b9d4b0b
coderHDY <coderHDY@users.noreply.github.com>
2022-10-19 14:51:34 +0000

    自动部署 f7c78cb2a2fe602ecd6d1685f8aa0798d3bd0b5f

--- 基础46-弹幕发射器/index.js
+++ 基础46-弹幕发射器/index.js
@@ -1,6 +1,8 @@
 const ipt = document.querySelector(".ipt");
 const submit = document.querySelector(".submit");
 const wsUrl = "http://localhost:8888/msg?m={msg}";
+// const wsUrl = "ws://hdy.gh520.xyz:8888/ws/msg?m={msg}";
+
 
 submit.addEventListener("click", () => {
     const val = ipt.value;
