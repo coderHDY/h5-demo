90ad1a507703c4e0d0823f06bad00b1146ad96ec
coderHDY <coderHDY@users.noreply.github.com>
2022-10-20 01:14:44 +0000

    自动部署 a62d66fb42c69d367cd729ab8d5c0aec602a9c57

--- 基础46-弹幕发射器/index.js
+++ 基础46-弹幕发射器/index.js
@@ -1,11 +1,19 @@
 const ipt = document.querySelector(".ipt");
 const submit = document.querySelector(".submit");
-const wsUrl = "http://localhost:8888/msg?m={msg}";
-// const wsUrl = "ws://hdy.gh520.xyz:8888/ws/msg?m={msg}";
+// const wsUrl = "http://localhost:8888/msg?m={msg}";
+const wsUrl = "https://hdy.gh520.xyz/ws/msg?m={msg}";
 
-
-submit.addEventListener("click", () => {
+const send = () => {
     const val = ipt.value;
     const msg = JSON.stringify({ text: val });
     fetch(wsUrl.replace("{msg}", msg)).catch(e => { });
+    ipt.value = "";
+}
+
+submit.addEventListener("click", send);
+
+ipt.addEventListener("keyup", e => {
+    if (e.keyCode === 13) {
+        send();
+    }
 })
