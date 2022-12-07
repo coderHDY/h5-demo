d40f3692aa131e2c4713df04318b1d0bfcea545c
coder-HDY <d_huang@dreamarts.co.jp>
2022-12-07 11:42:37 +0800

    烟花全屏

---
+++ src/canvas_烟花学习/index.js
@@ -0,0 +1,14 @@
+
+const goToFullScreen = (element = document.body) => {
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if (element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if (element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  } else if (element.webkitRequestFullscreen) {
+    element.webkitRequestFullScreen();
+  }
+};
+
+document.body.addEventListener("click", () => goToFullScreen())
