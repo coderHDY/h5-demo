a4cfc3e7753f97751109c546a9dbcf047ef87647
coder-HDY <d_huang@dreamarts.co.jp>
2022-12-16 23:36:51 +0800

    change address

--- src/01_聊天机器人/controlers/request.js
+++ src/01_聊天机器人/controlers/request.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://hdy.gh520.xyz";
+const BASE_URL = "http://47.92.141.56:8070";
 // const BASE_URL = "http://164.88.255.3:8070";
 
 const chatRobot = async (val) => await (await fetch(`${BASE_URL}/api/robot/${val}`, {
