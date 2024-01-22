a9052f465ffa3e90b5f8b1a45d841cbef94fb1d2
coderHDY <coderHDY@users.noreply.github.com>
2024-01-22 05:57:52 +0000

    自动部署 f92ec56c39a6630eecaa7591b5b6d28213832f0f

--- css_循环背景色2/index.js
+++ css_循环背景色2/index.js
@@ -3,10 +3,11 @@ const generateDateRaw = () => {
   let date = "";
   for (let i = 1; i <= 12; i++) {
     // TODO 待解决：数字宽度具体不确定，用span先代替
-    date += `<span class="month">`;
+    date += `<span class="month" style="z-index: ${i}">${i}月</span>`;
+    date += `<span class="days-wrapper">`;
     for (let i = 1; i <= 31; i++) {
       const d = `${i}`.padStart(2, "0");
-      date += `&nbsp;&nbsp;${d}日&nbsp;`;
+      date += `&nbsp;${d}日&nbsp&nbsp;`;
     }
     date += `</span>`;
   }
@@ -16,7 +17,7 @@ generateDateRaw();
 
 // sync scroll
 const syncScroll = () => {
-  const calender = document.querySelector(".calendar-wrapper");
+  const calender = document.querySelector(".calender");
   const taskScrollRoot = document.querySelector(".row-scroll-root");
   taskScrollRoot.addEventListener("scroll", () => {
     console.log("scroll");
