b3083a48c0c603834f09c785703ce637d9bfc997
coderHDY <986005715@qq.com>
2024-08-23 17:04:46 +0800

    feat: gsap

---
+++ src/gasp_轮播展示/index.js
@@ -0,0 +1,30 @@
+const rabbit1 = document.querySelector(".rabbit1");
+const rabbit2 = document.querySelector(".rabbit2");
+const rabbit3 = document.querySelector(".rabbit3");
+const rabbit4 = document.querySelector(".rabbit4");
+
+const asyncGsap = async (element, props) =>
+  new Promise((resolve, reject) => {
+    gsap.to(element, {
+      ...props,
+      onComplete: () => {
+        resolve();
+      },
+    });
+  });
+
+const loopShow = async (elements, duration) => {
+  for (element of elements) {
+    asyncGsap(element, {
+      duration,
+      display: "block",
+    });
+    await asyncGsap(element, {
+      duration: 1,
+      display: "none",
+    });
+  }
+  loopShow(elements, duration);
+};
+
+loopShow([rabbit1, rabbit2, rabbit3, rabbit4], 1);
